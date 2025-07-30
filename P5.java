045dd040d857b7adc49f19517706158a5af21fc7
TheFinalClass <papakiparikalaana@gmail.com>
2025-07-30 20:54:02 +0530

    P5.java

--- P5.java
+++ P5.java
@@ -1,6 +1,5 @@
 
-package darshan;
-
+package vatsal;
 import java.sql.*;
 
 
@@ -9,13 +8,17 @@ public class P5 {
         public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost/P4","root","");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost/P5","root","");
             Statement s = c.createStatement();
-            s.executeUpdate("insert into student values(96,'Vatsal','Surat',1234567890,'vatsal@gamail.com')");
-            s.executeUpdate("insert into student values(97,'Darshan','AA',1234567890,'darshan@gamail.com')");
-            s.executeUpdate("update student set  Name='shahaj' where Name='Vatsal'");
-            s.executeUpdate("delete from student where Enroll = '96'");
+            s.executeUpdate("insert into sturec values(95,'Naitik','Surat',1234567890,'naitik@gamail.com')");
+            s.executeUpdate("insert into sturec values(96,'Vatsal','Surat',1234567890,'vatsal@gamail.com')");
+            s.executeUpdate("insert into sturec values(97,'Darshan','Surat',1234567890,'darshan@gamail.com')");
+            s.executeUpdate("update sturec set  Name='shahaj' where Name='Darshan'");
+            s.executeUpdate("delete from sturec where Enroll = '96'");
       
+            s.close();
+            c.close();
+            
         } catch (Exception e) {
             System.out.println(e);
         }       
